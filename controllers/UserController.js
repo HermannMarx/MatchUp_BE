4f807a29b04ae3e30fa58ef4d58fd9b0c209cd98
Hermann Marx <marxhermann@web.de>
2021-03-11 20:22:08 +0100

    added mongoose query to filter users by latLng and specific interest

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,6 +11,30 @@ module.exports = {
     dbRes = await USER.find({ _id: id });
     res.json(dbRes);
   },
+  filterUsers: async (req, res) => {
+    const { latLng, activity } = req.body;
+    console.log("This is latLng:", latLng.lat);
+    console.log("This is minus:", latLng.lat - 0.05);
+    console.log("This is activity:", activity);
+
+    dbUsers = await USER.find({
+      $and: [
+        { interests: activity },
+        { "location.latLng.lng": { $gt: latLng.lng - 0.1 } },
+        { "location.latLng.lng": { $lt: latLng.lng + 0.1 } },
+        { "location.latLng.lat": { $gt: latLng.lat - 0.1 } },
+        { "location.latLng.lat": { $lt: latLng.lat + 0.1 } },
+      ],
+    });
+
+    /*    interests: activity,
+                   "location.latLng.lat": { $gt: latLng.lat - 0.05 },
+       "location.latLng.lat": { $lt: latLng.lat + 0.05 },
+       "location.latLng.lng": { $gt: latLng.lng - 0.05 },
+       "location.latLng.lng": { $lt: latLng.lng + 0.05 }, */
+    //  db.inventory.find( { $and: [ { price: { $ne: 1.99 } }, { price: { $exists: true } } ] } )
+    res.send(dbUsers);
+  },
   createUser: async (req, res) => {
     const { username, email, password, city, latLng, interests } = req.body;
     dbRes = await USER.create({
@@ -20,7 +44,10 @@ module.exports = {
       password: password,
       location: {
         city: city,
-        latLng: latLng,
+        latLng: {
+          lat: latLng.lat,
+          lng: latLng.lng,
+        }, //latLng,
       },
       interests: interests,
     });
