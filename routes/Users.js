418cb18e77adc95e25b28ec8d64dc2d6b40fd2d6
Hermann Marx <marxhermann@web.de>
2021-03-18 15:11:30 +0100

    fixed syntax error

--- routes/Users.js
+++ routes/Users.js
@@ -13,11 +13,11 @@ const verify = (req, res, next) => {
 };
 
 router.get("/users", userController.getAllUsers);
-router.post("/users" , userController.createUser);
+router.post("/users", userController.createUser);
 router.put("/users", userController.updateUser);
 router.put("/users/rmsport", userController.removeSport);
 router.put("/users/addsport", userController.addSport);
-router.post("/users/filter, userController.filterUsers);
+router.post("/users/filter", userController.filterUsers);
 router.post("/users/login", userController.login);
 router.get("/users/logout", userController.logout);
 router.get("/users/:id", userController.getUser);
